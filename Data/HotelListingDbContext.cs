5ea49eb5d75eed81a3a4f637f697362988d07cdc
KSCanLearn <kscanlearn@gmail.com>
2024-08-06 18:23:46 +0800

    Added Entity Framework and Setup+Created Database

---
+++ Data/HotelListingDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1001,
+                    Name = "Malaysia",
+                    ShortName = "MY",
+                },
+                new Country
+                {
+                    Id = 1002,
+                    Name = "Singapore",
+                    ShortName = "SG",
+                }
+            );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 2001,
+                    Name = "Hilton Hotel Malaysia",
+                    Address = "Petaling Jaya",
+                    CountryId = 1001,
+                    Rating = 4.2
+                },
+                new Hotel
+                {
+                    Id = 2002,
+                    Name = "Hilton Hotel Singapore",
+                    Address = "Clarke Clay",
+                    CountryId = 1002,
+                    Rating = 4
+                }
+            );
+        }
+    }
+}
---
+++ Migrations/20240806102153_SeededCountriesAndHotels.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeededCountriesAndHotels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1001, "Malaysia", "MY" },
+                    { 1002, "Singapore", "SG" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 2001, "Petaling Jaya", 1001, "Hilton Hotel Malaysia", 4.2000000000000002 },
+                    { 2002, "Clarke Clay", 1002, "Hilton Hotel Singapore", 4.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2001);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2002);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1001);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1002);
+        }
+    }
+}
